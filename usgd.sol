197ea1eb00ee5b090a6c39c104d66bf637477852
Hodlsage <hodlsage@protonmail.com>
2024-05-31 02:39:45 -0400

    Update usgd.sol

--- usgd.sol
+++ usgd.sol
@@ -124,17 +124,17 @@ contract ERC20 is Context, IERC20 {
         return true;
     }
 
-    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
-        return true;
-    }
+   function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+    return true;
+  }
 
-    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
-        return true;
-    }
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+    return true;
+  }
 
-    function _transfer(address sender, address recipient, uint256 amount) internal {
+    function _transfer(address sender, address recipient, uint256 amount) virtual internal {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
 
@@ -143,7 +143,7 @@ contract ERC20 is Context, IERC20 {
         emit Transfer(sender, recipient, amount);
     }
 
-    function _mint(address account, uint256 amount) internal {
+    function _mint(address account, uint256 amount) virtual internal {
         require(account != address(0), "ERC20: mint to the zero address");
 
         _totalSupply = _totalSupply.add(amount);
@@ -151,7 +151,7 @@ contract ERC20 is Context, IERC20 {
         emit Transfer(address(0), account, amount);
     }
 
-    function _burn(address account, uint256 amount) internal {
+    function _burn(address account, uint256 amount) virtual internal {
         require(account != address(0), "ERC20: burn from the zero address");
 
         _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
@@ -159,7 +159,7 @@ contract ERC20 is Context, IERC20 {
         emit Transfer(account, address(0), amount);
     }
 
-    function _approve(address owner, address spender, uint256 amount) internal {
+    function _approve(address owner, address spender, uint256 amount) virtual internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
 
@@ -167,7 +167,7 @@ contract ERC20 is Context, IERC20 {
         emit Approval(owner, spender, amount);
     }
 
-    function _burnFrom(address account, uint256 amount) internal {
+    function _burnFrom(address account, uint256 amount)virtual internal {
         _burn(account, amount);
         _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "ERC20: burn amount exceeds allowance"));
     }
@@ -176,53 +176,55 @@ contract ERC20 is Context, IERC20 {
 // ----------------------------------------------------------------------------
 // --- Abstract Contract Ownable 
 // ----------------------------------------------------------------------------
-abstract contract Ownable is Context {
-    address private _owner;
- 
-    error OwnableUnauthorizedAccount(address account);
- 
-    error OwnableInvalidOwner(address owner);
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
- 
-    constructor(address initialOwner) {
-        if (initialOwner == address(0)) {
-            revert OwnableInvalidOwner(address(0));
-        }
-        _transferOwnership(initialOwner);
+abstract contract Ownable is Context {
+  address private _owner;
+  error OwnableUnauthorizedAccount(address account);
+  error OwnableInvalidOwner(address owner);
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  constructor(address initialOwner) {
+
+    if (initialOwner == address(0)) {
+      revert OwnableInvalidOwner(address(0));
     }
- 
-    modifier onlyOwner() {
-        _checkOwner();
-        _;
+
+    _transferOwnership(initialOwner);
+  }
+
+  modifier onlyOwner() {
+    _checkOwner();
+    _;
+  }
+
+  function owner() public view virtual returns (address) {
+    return _owner;
+  }
+
+  function _checkOwner() internal view virtual {
+    if (owner() != _msgSender()) {
+      revert OwnableUnauthorizedAccount(_msgSender());
     }
+  }
+
+  function renounceOwnership() public virtual onlyOwner {
+    _transferOwnership(address(0));
+  }
+
+  function transferOwnership(address newOwner) public virtual onlyOwner {
  
-    function owner() public view virtual returns (address) {
-        return _owner;
+    if (newOwner == address(0)) {
+      revert OwnableInvalidOwner(address(0));
     }
- 
-    function _checkOwner() internal view virtual {
-        if (owner() != _msgSender()) {
-            revert OwnableUnauthorizedAccount(_msgSender());
-        }
-    }
- 
-    function renounceOwnership() public virtual onlyOwner {
-        _transferOwnership(address(0));
-    }
- 
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-        if (newOwner == address(0)) {
-            revert OwnableInvalidOwner(address(0));
-        }
-        _transferOwnership(newOwner);
-    }
- 
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
-    }
+
+    _transferOwnership(newOwner);
+  }
+
+  function _transferOwnership(address newOwner) internal virtual {
+    address oldOwner = _owner;
+    _owner = newOwner;
+   
+    emit OwnershipTransferred(oldOwner, newOwner);
+  }
 }
 
 // ----------------------------------------------------------------------------
@@ -302,7 +304,7 @@ contract PauserRole is Context {
     event PauserAdded(address indexed account);
     event PauserRemoved(address indexed account);
     Roles.Role private _pausers;
-    constructor ()  {
+    constructor () {
         _addPauser(_msgSender());
     }
 
@@ -377,147 +379,127 @@ contract Pausable is Context, PauserRole {
 // --- Contract USGD
 // ----------------------------------------------------------------------------
 
-abstract contract USGD is Ownable, ERC20, Pausable, Blacklistable {
+contract USGD is Ownable, ERC20, Pausable, Blacklistable {
     using SafeMath for uint256;
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    string public currency;
-    address public masterMinter;
-    address public pauser;
-    bool internal initialized;
-    mapping(address => uint256) internal balances;
-    mapping(address => mapping(address => uint256)) internal allowed;
-    uint256 internal totalSupply_ = 0;
-    mapping(address => bool) internal minters;
-    mapping(address => uint256) internal minterAllowed;
-    event Mint(address indexed minter, address indexed to, uint256 amount);
-    event Burn(address indexed burner, uint256 amount);
-    event MinterConfigured(address indexed minter, uint256 minterAllowedAmount);
-    event MinterRemoved(address indexed oldMinter);
-    event MasterMinterChanged(address indexed newMasterMinter);
-    
-    function initialize(
-        string memory _name,
-        string memory _symbol,
-        string memory _currency,
-        uint8 _decimals,
-        address _masterMinter,
-        address _pauser,
-        address _blacklister,
-        address _owner
-    ) public {
-        require(!initialized);
-        require(_masterMinter != address(0));
-        require(_pauser != address(0));
-        require(_blacklister != address(0));
-        require(_owner != address(0));
-        name = _name;
-        symbol = _symbol;
-        currency = _currency;
-        decimals = _decimals;
-        masterMinter = _masterMinter;
-        pauser = _pauser;
-        blacklister = _blacklister;
-        initialized = true;
-    }
 
-    modifier onlyMinters() {
-        require(minters[msg.sender] == true);
-        _;
-    }
+  mapping (address => uint256) private _balances;
 
-    function mint(address _to, uint256 _amount) whenNotPaused onlyMinters notBlacklisted(msg.sender) notBlacklisted(_to) public returns (bool) {
-        require(_to != address(0));
-        require(_amount > 0);
-        uint256 mintingAllowedAmount = minterAllowed[msg.sender];
-        require(_amount <= mintingAllowedAmount);
-        totalSupply_ = totalSupply_.add(_amount);
-        balances[_to] = balances[_to].add(_amount);
-        minterAllowed[msg.sender] = mintingAllowedAmount.sub(_amount);
-        emit Mint(msg.sender, _to, _amount);
-        emit Transfer(address(0), _to, _amount);
-        return true;
-    }
+  mapping (address => mapping (address => uint256)) private _allowances;
 
-    modifier onlyMasterMinter() {
-        require(msg.sender == masterMinter);
-        _;
-    }
+  uint256 private _totalSupply;
+  uint8 public _decimals;
+  string public _symbol;
+  string public _name;
 
-    function minterAllowance(address minter) public view returns (uint256) {
-        return minterAllowed[minter];
-    }
+  constructor() Ownable(msg.sender) {
+    _name = "USGD";
+    _symbol = "USGD";
+    _decimals = 0;
+     _totalSupply = 27000000000;
+     _balances[msg.sender] = _totalSupply;
 
-    function isMinter(address account) public view returns (bool) {
-        return minters[account];
-    }
+     emit Transfer(address(0), msg.sender, _totalSupply);
+}
 
-    function allowance(address owner, address spender) public view override returns (uint256) {
-        return allowed[owner][spender];
-    }
+  function getOwner() external view returns (address) {
+    return owner();
+  }
 
-    function totalSupply() public view override returns (uint256) {
-        return totalSupply_;
-    }
+  function decimals() external view returns (uint8) {
+    return _decimals;
+  }
 
-    function balanceOf(address account) public view override returns (uint256) {
-        return balances[account];
-    }
+  function symbol() external view returns (string memory) {
+    return _symbol;
+  }
 
-    function approve(address _spender, uint256 _value) whenNotPaused notBlacklisted(msg.sender) notBlacklisted(_spender) public override returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
-    }
+  function name() external view returns (string memory) {
+    return _name;
+  }
 
-    function transferFrom(address _from, address _to, uint256 _value) whenNotPaused notBlacklisted(_to) notBlacklisted(msg.sender) notBlacklisted(_from) public virtual override returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        emit Transfer(_from, _to, _value);
-        return true;
-    }
+  function totalSupply() public view  virtual override returns (uint256) {
+    return _totalSupply;
+  }
 
-    function transfer(address _to, uint256 _value) whenNotPaused notBlacklisted(msg.sender) notBlacklisted(_to) public override returns (bool) {
-        require(_to != address(0));
-        require(_value <= balances[msg.sender]);
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
-        return true;
-    }
+  function balanceOf(address account)  public view  virtual override returns (uint256) {
+    return _balances[account];
+  }
 
-    function configureMinter(address minter, uint256 minterAllowedAmount) whenNotPaused onlyMasterMinter public returns (bool) {
-        minters[minter] = true;
-        minterAllowed[minter] = minterAllowedAmount;
-        emit MinterConfigured(minter, minterAllowedAmount);
-        return true;
-    }
+  function transfer(address recipient, uint256 amount)  public virtual override returns (bool) {
+    _transfer(_msgSender(), recipient, amount);
+    return true;
+  }
 
-    function removeMinter(address minter) onlyMasterMinter public returns (bool) {
-        minters[minter] = false;
-        minterAllowed[minter] = 0;
-        emit MinterRemoved(minter);
-        return true;
-    }
+  function allowance(address owner, address spender)  public view  virtual override returns (uint256) {
+    return _allowances[owner][spender];
+  }
 
-    function burn(uint256 _amount) whenNotPaused onlyMinters notBlacklisted(msg.sender) public {
-        uint256 balance = balances[msg.sender];
-        require(_amount > 0);
-        require(balance >= _amount);
-        totalSupply_ = totalSupply_.sub(_amount);
-        balances[msg.sender] = balance.sub(_amount);
-        emit Burn(msg.sender, _amount);
-        emit Transfer(msg.sender, address(0), _amount);
-    }
+  function approve(address spender, uint256 amount)  public virtual override returns (bool) {
+    _approve(_msgSender(), spender, amount);
+    return true;
+  }
+ 
+  function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+    _transfer(sender, recipient, amount);
+    _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+    return true;
+  }
+ 
+  function increaseAllowance(address spender, uint256 addedValue) public virtual override returns(bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+    return true;
+  }
+ 
+  function decreaseAllowance(address spender, uint256 subtractedValue) public virtual override returns(bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+    return true;
+  }
+ 
+  function mint(uint256 amount) public onlyOwner returns (bool) {
+    _mint(_msgSender(), amount);
+    return true;
+  }
+ 
+  function burn(uint256 amount) public returns (bool) {
+    _burn(_msgSender(), amount);
+    return true;
+  }
+ 
+  function _transfer(address sender, address recipient, uint256 amount) override internal {
+    require(sender != address(0), "ERC20: transfer from the zero address");
+    require(recipient != address(0), "ERC20: transfer to the zero address");
 
-    function updateMasterMinter(address _newMasterMinter) onlyOwner public {
-        require(_newMasterMinter != address(0));
-        masterMinter = _newMasterMinter;
-        emit MasterMinterChanged(masterMinter);
-    }
+    _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+    _balances[recipient] = _balances[recipient].add(amount);
+    emit Transfer(sender, recipient, amount);
+  }
+ 
+  function _mint(address account, uint256 amount) override internal {
+    require(account != address(0), "ERC20: mint to the zero address");
+
+    _totalSupply = _totalSupply.add(amount);
+    _balances[account] = _balances[account].add(amount);
+    emit Transfer(address(0), account, amount);
+  }
+ 
+  function _burn(address account, uint256 amount) override internal {
+    require(account != address(0), "ERC20: burn from the zero address");
+
+    _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
+    _totalSupply = _totalSupply.sub(amount);
+    emit Transfer(account, address(0), amount);
+  }
+ 
+  function _approve(address owner, address spender, uint256 amount) override internal {
+    require(owner != address(0), "ERC20: approve from the zero address");
+    require(spender != address(0), "ERC20: approve to the zero address");
+
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+ 
+  function _burnFrom(address account, uint256 amount)override internal {
+    _burn(account, amount);
+    _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "ERC20: burn amount exceeds allowance"));
+  }
 }
